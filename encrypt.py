a75a3f4aef633266f732038439424c83bfeb0e70
Shlomi Zeltsinger <shultzi1@hotmail.com>
2018-04-22 08:35:40 +0000

    adding Flask

--- encrypt.py
+++ encrypt.py
@@ -1,14 +1,19 @@
 from umbral import umbral, keys, config
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+# config.set_default_curve()
 
-config.set_default_curve()
 
-input_private_key = "effIKT60Ei8M9EtLGb36Gt6+ZjXn7uj8okftqEjlKCE="
-input_plain_text = "hello"
+app = Flask(__name__)
+CORS(app)
 
-private_key = keys.UmbralPrivateKey.from_bytes(input_private_key)
+@app.route('/encrypt', methods=['POST'])
+def encrypt():
+    input_plain_text = request.form['plaintext']
+    input_private_key = "effIKT60Ei8M9EtLGb36Gt6+ZjXn7uj8okftqEjlKCE="
 
-public_key = private_key.get_pubkey()
+    private_key = keys.UmbralPrivateKey.from_bytes(input_private_key)
+    public_key = private_key.get_pubkey()
+    ciphertext, umbral_capsule = umbral.encrypt(public_key, input_plain_text.encode())
 
-ciphertext, umbral_capsule = umbral.encrypt(public_key, input_plain_text.encode())
-
-print("Chiper test: ", ciphertext.hex())
+    return jsonify({'ciphertext': ciphertext.hex()})
