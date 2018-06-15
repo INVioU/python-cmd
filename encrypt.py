5a96fc8ead96052c7ca096f67f438dc9b1d9c930
Shlomi Zeltsinger <shultzi1@hotmail.com>
2018-06-15 09:02:55 +0000

    Fix #8 Fix #9

--- encrypt.py
+++ encrypt.py
@@ -2,6 +2,7 @@ from umbral import umbral, keys, config
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 # config.set_default_curve()
+import base64
 
 
 app = Flask(__name__)
@@ -10,9 +11,13 @@ CORS(app)
 @app.route('/encrypt', methods=['POST'])
 def encrypt():
     input_plain_text = request.form['plaintext']
-    input_private_key = "effIKT60Ei8M9EtLGb36Gt6+ZjXn7uj8okftqEjlKCE="
+    input_private_key = request.form['priv']
+    # input_private_key = "effIKT60Ei8M9EtLGb36Gt6+ZjXn7uj8okftqEjlKCE="
 
-    private_key = keys.UmbralPrivateKey.from_bytes(input_private_key)
+    priv_b = bytes.fromhex(input_private_key)
+    private_key = base64.b64encode(priv_b)
+
+    private_key = keys.UmbralPrivateKey.from_bytes(private_key)
     public_key = private_key.get_pubkey()
     ciphertext, umbral_capsule = umbral.encrypt(public_key, input_plain_text.encode())
 
